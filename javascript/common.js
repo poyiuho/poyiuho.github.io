804686443c22015fbc86be08e4bfc9098ec1a1b7
Po Yiu Ho <poyiu.ho@stonybrook.edu>
2016-12-01 04:11:53 -0500

    Changed some stuff

--- javascript/common.js
+++ javascript/common.js
@@ -1,10 +1,54 @@
 $(document).ready(function() {
+    var $window = $(window),
+        $home = $('#home');
+    
+    $(window).scroll(function() {
+        var yPos = -($window.scrollTop() / 10);
+        var coords = '50%' + yPos + 'px';
+        
+        $home.css({
+            backgroundPosition: coords
+        });
+    });
+    
    var $skillbar = $(".skillbar");
     
     $skillbar.each(function() {
-        console.log($(this).data("percent"));
         $(this).animate({
             width: $(this).data("percent")
         });
     });
+    
+    var currentIndex = 0,
+        $items = $('#slider .pdf'),
+        $length = $items.length,
+        $prev = $('#prev'),
+        $next = $('#next');
+    
+    console.log($length);
+    
+    function showItem() {
+        var $item = $('#slider .pdf').eq(currentIndex);
+        $items.hide();
+        $item.css('display', 'inline-block');
+    }
+    
+    $next.click(function() {
+       currentIndex += 1;
+        if (currentIndex > $length - 1) {
+            currentIndex = 0;
+        }
+        
+        showItem();
+    });
+    
+    $prev.click(function() {
+       currentIndex -= 1;
+        if (currentIndex < 0) {
+            currentIndex = $length - 1;
+        }
+        showItem();
+    });
+    
+    
 });
